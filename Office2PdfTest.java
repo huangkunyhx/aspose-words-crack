74a126368c03318507dd46e6ce1756a11d92b43c
Kevin Huang <410594436@qq.com>
2024-06-19 12:00:40 +0800

    update

--- Office2PdfTest.java
+++ Office2PdfTest.java
@@ -1,20 +1,27 @@
 import com.aspose.words.Document;
-import com.aspose.words.SaveFormat;
+import com.aspose.words.FontSettings;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class Office2PdfTest {
     public static void main(String[] args) {
-        try {
-            String src = "文档.docx";
-            String dst = "文档.pdf";
-            long start = System.currentTimeMillis();
-            File file = new File(dst);
-            FileOutputStream os = new FileOutputStream(file);
-            Document doc = new Document(src);
-            doc.save(os, SaveFormat.PDF);
-            os.close();
+        long start = System.currentTimeMillis();
+        File file = new File("/Users/kevin/Desktop/1.pdf");
+
+        try (FileOutputStream os = new FileOutputStream(file);
+             InputStream fis = new FileInputStream("template.docx")) {
+            Document doc = new Document(fis);
+
+            FontSettings fontSettings = new FontSettings();
+            fontSettings.setFontsFolder("/Users/kevin/Desktop/管局投诉", false);
+            doc.setFontSettings(fontSettings);
+
+            doc.save(os, com.aspose.words.SaveFormat.PDF);
+            doc.cleanup();
+            os.flush();
             System.out.println("convert cost " + (System.currentTimeMillis() - start) + "ms");
         } catch (Exception e) {
             e.printStackTrace();
